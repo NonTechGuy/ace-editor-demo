2eeff2f3e9a1cc3ecfcbb7feba5f506a1d4e4f23
Craig Bartlett <craigmbartlett@gmail.com>
2020-11-26 11:06:21 +0200

    Reset master branch

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AceModule } from 'ngx-ace-wrapper';
+import { ACE_CONFIG } from 'ngx-ace-wrapper';
+import { AceConfigInterface } from 'ngx-ace-wrapper';
+import 'brace';
+import 'brace/mode/text';
+import 'brace/theme/github';
 
 const routes: Routes = [];
+const DEFAULT_ACE_CONFIG: AceConfigInterface = {
+};
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes),
+    AceModule],
+  providers: [
+    {
+      provide: ACE_CONFIG,
+      useValue: DEFAULT_ACE_CONFIG
+    }
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
